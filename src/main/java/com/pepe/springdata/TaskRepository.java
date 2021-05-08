666b6fd33d71424782817e1803d33c6159cf4ac2
PiotrPaprzycki <Avalok2015>
2021-05-08 12:22:55 +0200

    minor changes UI uplift

--- src/main/java/com/pepe/springdata/TaskRepository.java
+++ src/main/java/com/pepe/springdata/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.pepe.springdata;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByStatus(Status status);
+
+    Task findTaskById(Long id);
+
+    @Query("SELECT t FROM Task t WHERE t.status NOT LIKE 'DONE'")
+    List<Task> findNotDone();
 }
