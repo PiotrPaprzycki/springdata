3fff69bff5323a448ab5a59491ef470cb5b83248
PiotrPaprzycki <Avalok2015>
2021-05-15 09:33:34 +0200

    Migration script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO task(description, status, start_date) VALUES ('task1', 'TODO', '2020-01-02'), ('task2', 'TODO', '2017-05-03'), ('task3', 'IN_PROGRESS', '2021-03-10');
+INSERT INTO task(description, status, start_date, deadline, mandatory, assigne) VALUES
+('task1', 'TODO', '2020-01-02', '2020-01-02', true, 'no one'),
+('task2', 'TODO', '2017-05-03', '2018-05-03', false, 'no one'),
+('task3', 'IN_PROGRESS', '2021-03-10', '2021-03-10', true, 'no one');
